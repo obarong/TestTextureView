dd079bcee335f35a07bc63381c8120fe9c288d9f
obarong <dustinljr@gmail.com>
2020-07-28 17:24:14 +0800

    Add TextureView

--- app/src/main/java/com/obarong/testtextureview/MainActivity.java
+++ app/src/main/java/com/obarong/testtextureview/MainActivity.java
@@ -2,13 +2,55 @@ package com.obarong.testtextureview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.TextureView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener {
+
+    private TextureView mTextureView;
+    private Camera mCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mTextureView = new TextureView(this);
+        mTextureView.setSurfaceTextureListener(this);
+        setContentView(mTextureView);
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        mTextureView.setAlpha(0.5f);
+        mTextureView.setRotation(90.0f);
+
+        mCamera = Camera.open();
+
+        try {
+            mCamera.setPreviewTexture(surface);
+            mCamera.startPreview();
+        } catch (IOException ioe) {
+            // Something bad happened
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+        mCamera.stopPreview();
+        mCamera.release();
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
     }
 }
